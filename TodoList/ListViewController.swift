f80a76b07d38ae347f7ca1222adfdcc76e3492ff
Pavel Maltsev <maltsevpavel95@gmail.com>
2021-09-22 15:57:10 +0300

    Create alert for incorporate new task to arraylist

--- TodoList/ListViewController.swift
+++ TodoList/ListViewController.swift
@@ -9,17 +9,21 @@ import UIKit
 
 class ListViewController: UITableViewController {
     
-    let tasks = ["Cook diner","Clean house", "Buy food"]
+    var tasks = ["Cook diner","Clean house", "Buy food"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
     
+    //MARK: - TableView
+    
+    //tableview items coune (numberOfRowsInSection)
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return tasks.count
     }
     
+    //tableview init cell (cellForRowAt)
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: K.tableViewCellName, for: indexPath)
@@ -27,7 +31,45 @@ class ListViewController: UITableViewController {
         return cell
     }
     
+    //tableview on cell selected (didSelectRowAt)
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        if (tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark){
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        }else{
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
     
     
+    @IBAction func addBtnPressed(_ sender: UIBarButtonItem) {
+        //init alert
+        let alert = UIAlertController(title: "Add new task", message: "", preferredStyle: .alert)
+        
+        //adding textfield to alert
+        var textField = UITextField()
+        alert.addTextField(configurationHandler: {(alertTextField)in
+            alertTextField.placeholder = "enter name of task"
+            textField = alertTextField
+        })
+        
+        
+        //adding action to alert
+        let action = UIAlertAction(title: "add", style: .default, handler: {(action) in
+            if(!textField.text!.isEmpty){
+                self.tasks.append(textField.text!)
+            }else{
+                self.tasks.append("New task created")
+            }
+            
+            self.tableView.reloadData()
+        })
+        alert.addAction(action)
+        
+        //show alert
+        present(alert, animated: true, completion: nil)
+    }
     
 }
