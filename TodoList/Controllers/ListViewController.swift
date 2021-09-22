e2c6cc0d3dd65757d3f1cfabb4620377c4702e7a
Pavel Maltsev <maltsevpavel95@gmail.com>
2021-09-22 18:09:15 +0300

    cell checkbox issue [solved ]

--- TodoList/Controllers/ListViewController.swift
+++ TodoList/Controllers/ListViewController.swift
@@ -16,13 +16,13 @@ class ListViewController: UITableViewController {
         super.viewDidLoad()
         
         tasks.append(Task(title: "Cook diner" ,isCompleted: false))
-        tasks.append(Task(title: "Online meatig" ,isCompleted: false))
+        tasks.append(Task(title: "Online meatig" ,isCompleted: true))
         tasks.append(Task(title: "Clean room" ,isCompleted: false))
-
         
-        /*if let items = defaults.array(forKey: K.userDefaultsKey) as? [String]{
+        
+        if let items = defaults.array(forKey: K.userDefaultsKey) as? [Task]{
             tasks = items
-        }*/
+        }
         
     }
     
@@ -46,7 +46,7 @@ class ListViewController: UITableViewController {
             if(taskName.isEmpty){
                 taskName = K.defaultTaskName
             }
-        
+            
             self.tasks.append(Task(title: taskName, isCompleted: false))
             self.defaults.set(self.tasks, forKey: K.userDefaultsKey)
             self.tableView.reloadData()
@@ -68,18 +68,21 @@ class ListViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: K.tableViewCellName, for: indexPath)
-        cell.textLabel?.text = tasks[indexPath.row].title
-        if tasks[indexPath.row].isCompleted {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        }else{
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
+        let item = tasks[indexPath.row]
+        
+        cell.textLabel?.text = item.title
+        cell.accessoryType = item.isCompleted ? .checkmark : .none
         
         return cell
     }
     
     //tableview on cell selected (didSelectRowAt)
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        tasks[indexPath.row].isCompleted = !tasks[indexPath.row].isCompleted
+        
+        tableView.reloadData()
+        
         tableView.deselectRow(at: indexPath, animated: true)
     }
     
